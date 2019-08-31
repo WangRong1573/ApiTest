edb418994a683baf21ba267113e1c51c9da9c4f7
WangRong1573 <345001183@qq.com>
2019-08-31 10:39:28 +0800

    test switch windows

---
+++ Test01/AutoWebTest/src/main/java/com/api/testActions1.java
@@ -0,0 +1,59 @@
+package com.api;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * api练习：
+ * 实现从百度输入框输入refain 博客园，点击搜索，获取linktext元素，点击进入博客园，判断第一个文章是否可点击，点击
+ * @author rong.wang
+ */
+public class testActions1 {
+    WebDriver driver;
+    @BeforeTest
+    public void beforeTest(){
+        System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+        driver=new ChromeDriver();
+        driver.get("https://www.baidu.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+    @Test
+    public void testWindowSwitch(){
+        //定位百度输入框
+        driver.findElement(By.id("kw")).sendKeys("refain 博客园");
+        //定位搜索按钮并点击搜索
+        driver.findElement(By.id("su")).click();
+        //      对页面所有标题进行遍历，如果当前句柄不等于上一界面句柄，则切换至当前界面
+        String oldWindowHandle = driver.getWindowHandle();
+        driver.findElement(By.linkText("Refain - 博客园")).click();
+        Set<String> windowHandles = driver.getWindowHandles();
+        for (String newWindow:windowHandles) {
+            if (!oldWindowHandle.equals(newWindow)){
+                driver.switchTo().window(newWindow);
+                break;
+            }
+        }
+        driver.findElement(By.linkText("Selenium+java - PageFactory设计模式")).click();
+        System.out.println(driver.getTitle());
+        sleep(3);
+    }
+    @AfterTest
+    public void afterTest(){
+        driver.quit();
+    }
+    public void sleep(int i){
+        try {
+            Thread.sleep(1000*i);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
